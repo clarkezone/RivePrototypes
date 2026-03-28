490b8f3211fd44db65365c0bb02b5ed9e256fbbc
James Clarke <james@clarkezone.net>
2026-03-28 11:03:40 -0700

    Dino

--- RiveMenuPrototypes/RiveMenuPrototypes/ContentView.swift
+++ RiveMenuPrototypes/RiveMenuPrototypes/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 import RiveRuntime
 
 struct ContentView: View {
-    private var riveViewModel = RiveViewModel(fileName: "sausagefest", stateMachineName: "State Machine 1")
+    private var riveViewModel = RiveViewModel(fileName: "digging_dinosaurs", stateMachineName: "State Machine 1")
 
     var body: some View {
         riveViewModel.view()
-            .ignoresSafeArea()
+            .frame(maxWidth: 600, maxHeight: 600)
     }
 }
 
