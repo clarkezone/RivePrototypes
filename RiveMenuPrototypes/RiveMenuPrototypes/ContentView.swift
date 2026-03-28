82a2ecdb6cdb6fc95ae6ae49ec1e0e7d4c8c1760
James Clarke <james@clzn.dev>
2026-03-28 11:11:21 -0700

    Picker

--- RiveMenuPrototypes/RiveMenuPrototypes/ContentView.swift
+++ RiveMenuPrototypes/RiveMenuPrototypes/ContentView.swift
@@ -8,12 +8,51 @@
 import SwiftUI
 import RiveRuntime
 
+enum RiveAsset: String, CaseIterable, Identifiable {
+    case diggingDinosaurs = "digging_dinosaurs"
+    case sausagefest = "sausagefest"
+
+    var id: String { rawValue }
+
+    var displayName: String {
+        switch self {
+        case .diggingDinosaurs: "Digging Dinosaurs"
+        case .sausagefest: "Sausagefest"
+        }
+    }
+}
+
 struct ContentView: View {
-    private var riveViewModel = RiveViewModel(fileName: "digging_dinosaurs", stateMachineName: "State Machine 1")
+    @State private var selectedAsset: RiveAsset = .diggingDinosaurs
+
+    var body: some View {
+        VStack {
+            Picker("Animation", selection: $selectedAsset) {
+                ForEach(RiveAsset.allCases) { asset in
+                    Text(asset.displayName).tag(asset)
+                }
+            }
+            .pickerStyle(.menu)
+            .padding(.horizontal)
+
+            RiveAssetView(asset: selectedAsset)
+                .id(selectedAsset)
+                .padding(20)
+        }
+    }
+}
+
+struct RiveAssetView: View {
+    let asset: RiveAsset
+    private let riveViewModel: RiveViewModel
+
+    init(asset: RiveAsset) {
+        self.asset = asset
+        self.riveViewModel = RiveViewModel(fileName: asset.rawValue, stateMachineName: "State Machine 1")
+    }
 
     var body: some View {
         riveViewModel.view()
-            .frame(maxWidth: 600, maxHeight: 600)
     }
 }
 
